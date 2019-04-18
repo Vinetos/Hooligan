0cc34a6c4977575382d779494c8bf10d2c9ef9ac
Valentin Chassignol <Vinetosdev@gmail.com>
2019-04-18 16:27:11 +0200

    feat: Add HooliganMessage Wrapper and LoginMessage

---
+++ src/main/java/com/safexty/hooligan/network/messages/HooliganMessage.java
@@ -0,0 +1,61 @@
+package com.safexty.hooligan.network.messages;
+
+import flex.messaging.messages.AbstractMessage;
+
+/**
+ * File <b>HooliganMessage</b> located on com.safexty.hooligan.network.messages
+ * Hooligan is a part of SAFExTY Project.
+ * <p>
+ *   ____    _    _____ _____      _______   __       :::     ::: ::::::::::: ::::    ::: :::::::::: ::::::::::: ::::::::   ::::::::
+ * / ___|  / \  |  ___| ____|_  _|_   _\ \ / /      :+:     :+:     :+:     :+:+:   :+: :+:            :+:    :+:    :+: :+:    :+:
+ * \___ \ / _ \ | |_  |  _| \ \/ / | |  \ V /     +:+     +:+     +:+     :+:+:+  +:+ +:+            +:+    +:+    +:+ +:+
+ *   ___) / ___ \|  _| | |___ >  <  | |   | |     +#+     +:+     +#+     +#+ +:+ +#+ +#++:++#       +#+    +#+    +:+ +#++:++#++
+ * |____/_/   \_\_|   |_____/_/\_\ |_|   |_|     +#+   +#+      +#+     +#+  +#+#+# +#+            +#+    +#+    +#+        +#+
+ *                                               #+#+#+#       #+#     #+#   #+#+# #+#            #+#    #+#    #+# #+#    #+#
+ *                and                             ::::    ::::::::::: ::::   ::::  ::::::::::    :::    ::::::::::  ::::::::        .fr
+ *
+ * <p>
+ * Copyright (c) 2019 SAFExTY.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * <p>
+ *
+ * @author Valentin Chassignol (Valentin), {@literal <contact@vinetos.fr>}, https://www.vinetos.fr
+ * Created the 18/04/2019 14:00
+ * @version 0.0.1
+ * @since 0.0.1
+ *
+ * <p>
+ * Represent a default message
+ */
+public abstract class HooliganMessage<T extends AbstractMessage> {
+
+    private final String operation;
+    private final String destination;
+
+    public HooliganMessage(String operation, String destination) {
+        this.operation = operation;
+        this.destination = destination;
+    }
+
+    public abstract T getRawMessage();
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+}
